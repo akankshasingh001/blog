529f2f3524f704083fa09d029084c51f6c8f0c0d
Akanksha Singh <singhakanksha001@gmail.com>
2020-04-03 19:44:20 -0400

    refactor of fetching unique id

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,9 +4,15 @@ import jsonPlaceHolder from '../apis/jsonPlaceHolder';
 //Alternate method for lodash memoize function
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
+  // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  // userIds.forEach(id => dispatch(fetchUser(id)));
 
-  const userIds = _.uniq(_.map(getState().posts, 'userId'));
-  userIds.forEach(id => dispatch(fetchUser(id)));
+  //refactor of getting userIds with lodash
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
 };
 
 export const fetchPosts = () => async dispatch => {
