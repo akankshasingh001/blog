69b85ef337c0867cad91c1e1c9a4d6254bca8a85
Akanksha Singh <singhakanksha001@gmail.com>
2020-04-03 19:38:22 -0400

    logic added for alternate of memoize function to get unique user id

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,14 @@
 import _ from 'lodash';
 import jsonPlaceHolder from '../apis/jsonPlaceHolder';
 
+//Alternate method for lodash memoize function
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  userIds.forEach(id => dispatch(fetchUser(id)));
+};
+
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceHolder.get('/posts');
   dispatch({
@@ -9,13 +17,21 @@ export const fetchPosts = () => async dispatch => {
   });
 };
 
-export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
-
-//using memoize function of lodash to load one unique id of user only once
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = id => async dispatch => {
   const user = await jsonPlaceHolder.get(`/users/${id}`);
   dispatch({
     type: 'FETCH_USER',
     payload: user.data
   });
-});
+};
+
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+
+// //using memoize function of lodash to load one unique id of user only once
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const user = await jsonPlaceHolder.get(`/users/${id}`);
+//   dispatch({
+//     type: 'FETCH_USER',
+//     payload: user.data
+//   });
+// });
